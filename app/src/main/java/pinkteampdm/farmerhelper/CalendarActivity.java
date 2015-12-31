e8e7a82c67c286b166cfefd8a9ae69d6a849eb9d
vscm21 <sophie.vm2189@gmail.com>
2015-12-31 01:55:20 +0000

    Notification is on ;)

--- app/src/main/java/pinkteampdm/farmerhelper/CalendarActivity.java
+++ app/src/main/java/pinkteampdm/farmerhelper/CalendarActivity.java
@@ -1,9 +1,12 @@
 package pinkteampdm.farmerhelper;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -11,27 +14,30 @@ public class CalendarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        NotificationManager notificationManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+
+        Intent intent = new Intent(this, PlantActivity.class);
+// use System.currentTimeMillis() to have a unique ID for the pending intent
+        PendingIntent pIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
+
+       /* Intent intent2 = new Intent(this, LocationActivity.class);
+        PendingIntent pIntent2 = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);*/
+
+// build notification
+// the addAction re-use the same intent to keep the example short
+        Notification n  = new Notification.Builder(this)
+                .setContentTitle("New mail from " + "VANESSA@gmail.com")
+                .setContentText("Subject")
+                .setSmallIcon(R.drawable.picket)
+                .setContentIntent(pIntent)
+                .setAutoCancel(true)
+                .addAction(R.drawable.picket, "Call",pIntent)
+                .addAction(R.drawable.farmgps2, "More", pIntent)
+                .addAction(R.drawable.coordenadas, "And more", pIntent).build();
+
+        notificationManager.notify(0, n);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_calendar, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
